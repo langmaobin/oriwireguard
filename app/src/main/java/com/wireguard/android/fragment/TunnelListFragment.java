607809d374bd4a9e64a38d1f28e7542bae931f35
langmb <1047544883@qq.com>
2018-12-18 14:14:25 +0800

    封装参数

--- app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.java
+++ app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.java
@@ -285,23 +285,23 @@ public class TunnelListFragment extends BaseFragment {
     public void onRequestCreatelocal(@SuppressWarnings("unused") final View view) {
         try{
             //1:封装peer
-//            Peerlocal peerlocal=new Peerlocal();
-//            peerlocal.setPublicKey("RAg5S1r3VGqq+emlo5OwhHSLv5Zz0YUoDh17AfenlXo=");
-//            peerlocal.setEndpoint("123.206.67.247:51820");
-//            peerlocal.setAllowedIps("0.0.0.0/0");
-//            Collection<Peer> resolvedPeers = new ArrayList<>();
-//            resolvedPeers.add(peerlocal.resolve());
-//            //2:封装interface
-//            Interfacelocal interfacelocal=new Interfacelocal();
-//            interfacelocal.setPrivateKey("IBQRmUDMANH0f5gfWVvLa3GiU8YhKtwm5CwSu8l+RHs=");
-//            interfacelocal.setAddresses("10.100.1.4");
-//            interfacelocal.setDnsServers("8.8.8.8");
-//            interfacelocal.setListenPort("24");
-//            Config newConfig=  new Config.Builder()
-//                    .setInterface(interfacelocal.resolve())
-//                    .addPeers(resolvedPeers)
-//                    .build();
-//            final TunnelManager manager = Application.getTunnelManager();
+            Peerlocal peerlocal=new Peerlocal();
+            peerlocal.setPublicKey("RAg5S1r3VGqq+emlo5OwhHSLv5Zz0YUoDh17AfenlXo=");
+            peerlocal.setEndpoint("123.206.67.247:51820");
+            peerlocal.setAllowedIps("0.0.0.0/0");
+            Collection<Peer> resolvedPeers = new ArrayList<>();
+            resolvedPeers.add(peerlocal.resolve());
+            //2:封装interface
+            Interfacelocal interfacelocal=new Interfacelocal();
+            interfacelocal.setPrivateKey("IBQRmUDMANH0f5gfWVvLa3GiU8YhKtwm5CwSu8l+RHs=");
+            interfacelocal.setAddresses("10.100.1.4");
+            interfacelocal.setDnsServers("8.8.8.8");
+            interfacelocal.setListenPort("24");
+            Config newConfig=  new Config.Builder()
+                    .setInterface(interfacelocal.resolve())
+                    .addPeers(resolvedPeers)
+                    .build();
+            final TunnelManager manager = Application.getTunnelManager();
 //        manager.create(binding.getName(), newConfig)
 //                .whenComplete(this::onTunnelCreated);
         }catch (Exception e){
